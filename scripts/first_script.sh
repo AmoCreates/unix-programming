5988995f517fb8d068dfa3fdad95eea6fb64cd33
AmoCreates <AmoCreates@outlook.com>
2026-05-07 04:57:17 +0000

    Shell script basics upto function

---
+++ scripts/first_script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# The line above is the "Shebang" - it tells Linux to use the Bash interpreter
+echo "Hello from the Unix Shell!"
+echo "The current time is: $(date)"
+echo "My username is: $USER"
